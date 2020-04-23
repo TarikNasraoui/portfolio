8dcffecdff6ecfa51131bfa8133c9b8e845738fa
Tarik nasraoui <tariik.nasraoui@gmail.com>
2020-04-23 15:34:43 +0000

    backend

---
+++ backEnd/server.js
@@ -0,0 +1,62 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+const http = require('http');
+//Models
+require('./model/user');
+
+//Routes
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+//Configs
+require('./config/connection');
+
+
+var app = express();
+
+
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+//app.use(cookieParser());
+
+
+app.use('/', indexRouter);
+app.use('/api/users', usersRouter);
+
+
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  console.log(err);
+
+  // render the error page
+  res.status(err.status || 500);
+  res.end(err);
+});
+
+/**
+ * Get port from environment and store in Express.
+ */
+const port = process.env.PORT || '3000';
+app.set('port', port);
+
+/**
+ * Create HTTP server.
+ */
+const server = http.createServer(app);
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+server.listen(port, () => console.log(`API running on localhost:${port}`));
+
+module.exports = app;
