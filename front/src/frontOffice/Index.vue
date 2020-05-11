924078974a95f1ca70a7e6a69878c2e8fdb7e7b9
Tarik nasraoui <tariik.nasraoui@gmail.com>
2020-05-11 23:50:34 +0000

    adding main

--- front/src/frontOffice/Index.vue
+++ front/src/frontOffice/Index.vue
@@ -3,15 +3,18 @@
 		<div class="container-wrap">
             <a href="#" class="js-colorlib-nav-toggle colorlib-nav-toggle" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar"><i></i></a>
             <Aside />
+            <Main />
 		</div>
     </div>
 </template>
 <script>
     import Aside from './Aside'
+    import Main from './Main'
     export default {
         name: "Index",
         components: {
-            Aside
+            Aside,
+            Main
         }
     }
 </script>
